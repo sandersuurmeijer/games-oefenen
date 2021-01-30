fd9907321853da9644076a6567d39215792498aa
sandersuurmeijer <152558@csgaugustinus.nl>
2021-01-30 11:00:40 +0000

    begin

---
+++ JS/H1/H1O01.js
@@ -0,0 +1,22 @@
+function setup() {
+  canvas = createCanvas(1000,500);
+  background('orange');
+  canvas.parent('processing');
+  noLoop();
+}
+
+function draw() {
+  // groene cirkel zonder rand
+  
+  noStroke();
+  fill('green');
+  ellipse(300,200,400);
+  
+  // witte rechthoek met rode rand
+  
+  stroke('red');
+  fill('white');
+  strokeWeight(10);
+  rect(650,100,250,300);  
+}
+// python3 -m http.server 3000 &
